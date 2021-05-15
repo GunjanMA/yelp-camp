b971ae5b27a850789d8b64b9620735911cb414e0
Gunjan <gunjan13082000@gmail.com>
2021-05-15 18:15:33 +0530

    CRUD operations done

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app=express();
 const path=require('path');
 const mongoose=require('mongoose');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override'); 
+//const campground = require("./models/campground");
+
+app.use(methodOverride('_method'));
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -45,10 +49,28 @@ app.get("/campgrounds/:id", async (req,res)=>{
 
 app.use(express.urlencoded({extended: true}));
 
+app.get("/campgrounds/:id/edit", async (req,res)=>{
+    const campground= await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+});
+
+
 app.post("/campgrounds", async (req,res)=>{
     const campground=new Campground(req.body.campground);
     await campground.save();
-    res.redirect("/campgrounds/${campground._id}");
+    res.redirect("/campgrounds/${ campground._id }");
+});
+
+app.put('/campgrounds/:id', async (req,res)=>{
+    const { id } = req.params;
+    const campground=await Campground.findByIdAndUpdate(id,{...req.body.campground});
+    res.redirect("/campgrounds/${ campground._id }");
+});
+
+app.delete('/campgrounds/:id', async (req,res)=>{
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 
 process.env.PORT=3000;
