377e44e8a7fa3072562ff308efcafbfdadc58f45
Gunjan <gunjan13082000@gmail.com>
2021-06-25 01:14:01 +0530

    added image upload, edit and delete

--- seeds/index.js
+++ seeds/index.js
@@ -28,9 +28,18 @@ const seedDB = async () => {
             author: '60c3d2532eeb513b68ac80fe',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
-            price
+            price,
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/yelpcamp-gunjan/image/upload/v1623530694/sample.jpg',
+                    filename: 'YelpCamp/sample'
+                },
+                {
+                    url: 'https://res.cloudinary.com/yelpcamp-gunjan/image/upload/v1623530694/sample.jpg',
+                    filename: 'YelpCamp/sample'
+                }
+            ]
         })
         await camp.save();
     }
